12e9918b6e12898e477e6d2c924aea0c0b1d7a8d
Denis Koronchik <denis.koronchik@gmail.com>
2014-01-22 17:11:55 +0300

    small fix in dom structure for scs

--- scs/src/scn-output.js
+++ scs/src/scn-output.js
@@ -92,8 +92,7 @@ SCs.SCnOutput.prototype = {
                 var contourTree = this.tree.subtrees[treeNode.element.addr];
                 if (contourTree) {
                     output += '<div class="scs-scn-element scs-scn-contour scs-scn-field">' //sc_addr="' + treeNode.element.addr + '">'
-                            + this.subtreeToHtml(contourTree)
-                            + '</div>';
+                            + this.subtreeToHtml(contourTree);
                 } else {
                     output += this.treeNodeElementHtml(treeNode);
                     output += childsToHtml();
